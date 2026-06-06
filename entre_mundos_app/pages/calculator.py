b95f2727e56a08c13d066b79b47d80c3b57b4ee3
Marcus Reis <marcusfilipesr@gmail.com>
2026-06-06 13:08:31 -0300

    implementado o repasse das taxas

--- entre_mundos_app/pages/calculator.py
+++ entre_mundos_app/pages/calculator.py
@@ -7,6 +7,7 @@ import streamlit as st
 from datetime import datetime
 from common import default_page_config
 
+
 def save_output(data):
     output = io.BytesIO()
     xls = pd.ExcelWriter(output, engine="xlsxwriter")
@@ -14,15 +15,16 @@ def save_output(data):
     xls.close()
     return output.getvalue()
 
+
 def dynamic_input_data_editor(data, key, **_kwargs):
-    changed_key = f'{key}_khkhkkhkkhkhkihsdhsaskskhhfgiolwmxkahs'
-    initial_data_key = f'{key}_khkhkkhkkhkhkihsdhsaskskhhfgiolwmxkahs__initial_data'
+    changed_key = f"{key}_khkhkkhkkhkhkihsdhsaskskhhfgiolwmxkahs"
+    initial_data_key = f"{key}_khkhkkhkkhkhkihsdhsaskskhhfgiolwmxkahs__initial_data"
 
     def on_data_editor_changed():
-        if 'on_change' in _kwargs:
-            args = _kwargs['args'] if 'args' in _kwargs else ()
-            kwargs = _kwargs['kwargs'] if 'kwargs' in _kwargs else  {}
-            _kwargs['on_change'](*args, **kwargs)
+        if "on_change" in _kwargs:
+            args = _kwargs["args"] if "args" in _kwargs else ()
+            kwargs = _kwargs["kwargs"] if "kwargs" in _kwargs else {}
+            _kwargs["on_change"](*args, **kwargs)
         st.session_state[changed_key] = True
 
     if changed_key in st.session_state and st.session_state[changed_key]:
@@ -31,11 +33,13 @@ def dynamic_input_data_editor(data, key, **_kwargs):
     else:
         st.session_state[initial_data_key] = data
     __kwargs = _kwargs.copy()
-    __kwargs.update({'data': data, 'key': key, 'on_change': on_data_editor_changed})
+    __kwargs.update({"data": data, "key": key, "on_change": on_data_editor_changed})
     return st.data_editor(**__kwargs)
+
 
 def editar_taxa():
     st.session_state["alterar_taxa"] = not st.session_state["alterar_taxa"]
+
 
 def card_valor(titulo, texto, width="stretch", height=120, color="#31333f"):
     if isinstance(width, int):
@@ -49,10 +53,13 @@ def card_valor(titulo, texto, width="stretch", height=120, color="#31333f"):
                     <a style="font-size:24px;font-weight:bold;color:{color}">{texto}</a>
                 </div>
             """,
-            unsafe_allow_html=True
+            unsafe_allow_html=True,
         )
 
-def card_valor_lucro(titulo, texto, lucro, width="stretch", height=120, color="#31333f"):
+
+def card_valor_lucro(
+    titulo, texto, lucro, width="stretch", height=120, color="#31333f"
+):
     if isinstance(width, int):
         height = width
     with st.container(border=True, width=width, height=height):
@@ -63,13 +70,16 @@ def card_valor_lucro(titulo, texto, lucro, width="stretch", height=120, color="#
                     </br>
                     <a style="font-size:24px;font-weight:bold;color:{color}">{texto}</a>
                     </br>
-                    <a style="font-size:14px;font-weight:bold;color:{'#FF6E6E' if lucro < 0 else '#5BBF54'}">R${'' if lucro < 0 else '+'}{lucro:.2f}</a>
+                    <a style="font-size:14px;font-weight:bold;color:{"#FF6E6E" if lucro < 0 else "#5BBF54"}">R${"" if lucro < 0 else "+"}{lucro:.2f}</a>
                 </div>
             """,
-            unsafe_allow_html=True
+            unsafe_allow_html=True,
         )
 
-def card_valor_lucro_parcela(titulo, recebido, pago, lucro, width="stretch", height=150, color="#31333f"):
+
+def card_valor_lucro_parcela(
+    titulo, recebido, pago, lucro, width="stretch", height=150, color="#31333f"
+):
     if isinstance(width, int):
         height = width
     with st.container(border=True, width=width, height=height):
@@ -82,33 +92,47 @@ def card_valor_lucro_parcela(titulo, recebido, pago, lucro, width="stretch", hei
                     </br>
                     <a style="font-size:24px;font-weight:bold;color:{color}">{recebido}</a>
                     </br>
-                    <a style="font-size:14px;font-weight:bold;color:{'#FF6E6E' if lucro < 0 else '#5BBF54'}">R${'' if lucro < 0 else '+'}{lucro:.2f}</a>
+                    <a style="font-size:14px;font-weight:bold;color:{"#FF6E6E" if lucro < 0 else "#5BBF54"}">R${"" if lucro < 0 else "+"}{lucro:.2f}</a>
                 </div>
             """,
-            unsafe_allow_html=True
+            unsafe_allow_html=True,
         )
 
-def calculo_parcelado(valor, n_parcelas, taxa, taxa_adiantamento):
+
+def calcular_parcelado(valor_pago, n_parcelas, taxa, taxa_adiantamento, repassar):
     taxa /= 100
     taxa_adiantamento /= 100
 
     dias_comercial = 30
-    valor_parcela = valor / n_parcelas
+    valor_parcela = valor_pago / n_parcelas
     valor_taxa = valor_parcela * taxa
 
-    valor_final = 0
+    valor_receber = 0
     for parcela in range(1, n_parcelas + 1):
-        descontos = ((parcela * (taxa_adiantamento * ((dias_comercial * parcela) - 1) / (dias_comercial * parcela)) * (valor_parcela - valor_taxa)) + valor_taxa)
-        valor_final += (valor_parcela - descontos)
+        desconto = (
+            parcela
+            * (
+                taxa_adiantamento
+                * ((dias_comercial * parcela) - 1)
+                / (dias_comercial * parcela)
+            )
+            * (valor_parcela - valor_taxa)
+        ) + valor_taxa
+        if repassar:
+            valor_pago += desconto
+            valor_receber += valor_parcela
+        else:
+            valor_receber += valor_parcela - desconto
 
-    return valor_final
+    return valor_pago, valor_receber
+
 
 def main():
     qtd_guias = 2
-    
+
     default_page_config()
 
-    left, center, right = st.columns([.2, .6, .2], vertical_alignment="top")
+    left, center, right = st.columns([0.2, 0.6, 0.2], vertical_alignment="top")
 
     left.write("**Configurações :material/settings:**")
     config_container = left.container(border=True)
@@ -157,7 +181,7 @@ def main():
         label="Incluir o adicional de 10%",
         key="desconto_10",
         value=True,
-        help="Escolha se irá incluir no valor do evento o desconto de 10% para as primeiras Entre Munders."
+        help="Escolha se irá incluir no valor do evento o desconto de 10% para as primeiras Entre Munders.",
     )
 
     left.write("**Informações do Evento**")
@@ -184,13 +208,15 @@ def main():
         label="Diária para 1 guia",
         key="diaria_uma_guia",
         value=False,
-        help="Se o valor da diária será para uma só (Viki ou Laura). O padrão é considerar a diária para duas."
+        help="Se o valor da diária será para uma só (Viki ou Laura). O padrão é considerar a diária para duas.",
     )
     left.write("**Carregar Planilha de Custos**")
     st.session_state["custos_carregado"] = left.file_uploader(
         label="Insira planilha de custos que deseja",
-        type=['xlsx',],
-        key="uploader_custos"
+        type=[
+            "xlsx",
+        ],
+        key="uploader_custos",
     )
 
     if st.session_state["custos_carregado"] is not None:
@@ -210,9 +236,9 @@ def main():
             column_config={
                 "nome": st.column_config.TextColumn(
                     label="Nome",
-                    help="Adicione aqui os custos que não dependem da quantidade de pessoas envolvidas no evento. Por exemplo: "
+                    help="Adicione aqui os custos que não dependem da quantidade de pessoas envolvidas no evento. Por exemplo: ",
                 ),
-                "valor":st.column_config.NumberColumn(
+                "valor": st.column_config.NumberColumn(
                     label="Valor",
                     default=0.0,
                     format="R$ %.2f",
@@ -220,12 +246,12 @@ def main():
                 "depende": st.column_config.CheckboxColumn(
                     label="Cobrado por pessoa?",
                     default=False,
-                )
+                ),
             },
             num_rows="dynamic",
         )
         st.session_state["custos_file"] = save_output(custos_atualizado)
-    
+
         st.download_button(
             label=":material/download: Planilha de Custos",
             key="baixar_custos",
@@ -243,21 +269,23 @@ def main():
             custo_pessoa += valor
         else:
             custo_fixo += valor
-    
+
     taxa_incluir = 0
     if desconto_10:
         taxa_incluir += 10
-    
+
     custo_diaria = qtd_guias * valor_diaria * qtd_dias
     custo_fixo += custo_diaria
-    if custo_fixo == custo_diaria: # Vazio
+    if custo_fixo == custo_diaria:  # Vazio
         custo_fixo = 0
-    
+
     qtd_pessoas = min_pessoas + qtd_guias
     custo_variavel = custo_pessoa * qtd_pessoas
     custo_total = custo_variavel + custo_fixo
     custo_total_unit = custo_total / min_pessoas
-    valor_unitario = ((custo_variavel * (1 + margem_lucro / 100)) + custo_fixo) / min_pessoas
+    valor_unitario = (
+        (custo_variavel * (1 + margem_lucro / 100)) + custo_fixo
+    ) / min_pessoas
     valor_unitario /= (1 - taxa_incluir / 100) if desconto_10 else 1
 
     center_2.write("**Resumo**")
@@ -273,36 +301,68 @@ def main():
     center.divider()
     center.write("**Parcelamento**")
     lista_parcelas = [n for n in range(1, 14)]
-    center_col1, center_col2, center_col3, center_col4 = center.columns(4, vertical_alignment="top")
+    center_col1, center_col2, center_col3, center_col4 = center.columns(
+        4, vertical_alignment="top"
+    )
     qtd_parcelas = center_col1.selectbox(
         label="Quantidade de Parcelas",
         options=lista_parcelas,
         key="qtd_parcelas",
         index=0,
     )
+    repassar_parcela = center_col1.toggle(
+        label="Repassar a taxa",
+        value=False,
+        key="repassar_parcela",
+        help="O valor total das taxas será incluído no valor a ser pago pela Entre Munder.",
+    )
     valor_entre_munders = valor_unitario * (1 - 0.1)
 
     with center_col2:
-        valor_final = calculo_parcelado(valor_unitario, qtd_parcelas, taxa_cartao, taxa_adiantamento)
-        lucro = valor_final - custo_total_unit
-        card_valor_lucro_parcela(f"Parcelado {qtd_parcelas}x", f"R${valor_final:.2f}", f"R${valor_unitario:.2f}", lucro)
+        valor_pago, valor_receber = calcular_parcelado(
+            valor_unitario,
+            qtd_parcelas,
+            taxa_cartao,
+            taxa_adiantamento,
+            repassar_parcela,
+        )
+        lucro = valor_receber - custo_total_unit
+        card_valor_lucro_parcela(
+            titulo=f"Parcelado {qtd_parcelas}x",
+            recebido=f"R${valor_receber:.2f}",
+            pago=f"R${valor_pago:.2f}",
+            lucro=lucro,
+        )
     with center_col3:
-        valor_final = calculo_parcelado(valor_entre_munders, qtd_parcelas, taxa_cartao, taxa_adiantamento)
-        lucro = valor_final - custo_total_unit
-        card_valor_lucro_parcela(f"EM 10% + Parcelado {qtd_parcelas}x", f"R${valor_final:.2f}", f"R${valor_entre_munders:.2f}", lucro)
+        valor_pago, valor_receber = calcular_parcelado(
+            valor_entre_munders,
+            qtd_parcelas,
+            taxa_cartao,
+            taxa_adiantamento,
+            repassar_parcela,
+        )
+        lucro = valor_receber - custo_total_unit
+        card_valor_lucro_parcela(
+            titulo=f"EM 10% + Parcelado {qtd_parcelas}x",
+            recebido=f"R${valor_receber:.2f}",
+            pago=f"R${valor_pago:.2f}",
+            lucro=lucro,
+        )
 
     right.write("**Valores Calculados**")
     with right:
         lucro = valor_unitario - custo_total_unit
-        card_valor_lucro("Valor do Evento", f"R${valor_unitario:.2f}", lucro, color="#3892FF")
+        card_valor_lucro(
+            "Valor do Evento", f"R${valor_unitario:.2f}", lucro, color="#3892FF"
+        )
         valor_pix = valor_unitario * (1 - 0.05)
         lucro = valor_pix - custo_total_unit
         card_valor_lucro("Pix 5%", f"R${valor_pix:.2f}", lucro)
         lucro = valor_entre_munders - custo_total_unit
         card_valor_lucro("EntreMunders 10%", f"R${valor_entre_munders:.2f}", lucro)
-        valor_final = valor_entre_munders * (1 - 0.05)
-        lucro = valor_final - custo_total_unit
-        card_valor_lucro("EntreMunders 10% + Pix", f"R${valor_final:.2f}", lucro)
+        valor_receber = valor_entre_munders * (1 - 0.05)
+        lucro = valor_receber - custo_total_unit
+        card_valor_lucro("EntreMunders 10% + Pix", f"R${valor_receber:.2f}", lucro)
 
 
 if __name__ == "__main__":
